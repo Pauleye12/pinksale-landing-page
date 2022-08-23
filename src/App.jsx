64436d62e2e40f56e65f6b3dd141a2eb92bd90e5
pauleye <orepaul12@gmail.com>
2022-08-23 14:25:53 +0100

    further edits

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,20 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
-import Introduction from './components/introduction'
-import ToggleState from './components/Togglestate';
-import RenderPermanent from './components/renderPermanent';
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+import Introduction from "./components/introduction";
+import ToggleState from "./components/Togglestate";
+import RenderPermanent from "./components/renderPermanent";
 
 function App() {
-
   return (
-   <div className=' bg-color '>
-    <ToggleState/>
-    <RenderPermanent/>
-    <Introduction/>
-   </div>
-  )
+    <div className=" bg-color ">
+      <ToggleState />
+      <RenderPermanent />
+      <Introduction />
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/components/permanentSideBar.jsx
+++ src/components/permanentSideBar.jsx
@@ -1,21 +1,22 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 class PermanentSideBar extends Component {
-    constructor(props) {
-        super(props);
-      }
-      render() {
-        return (
-          <div className="side-bar-items">
-            <a href="" className="side-bar-link">
-              <div className=" side-bar-items-content d-flex justify-content-start align-items-center">
-                {this.props.content.logo}
-               
-              </div>
-            </a>
-          </div>
-        );
-      }
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div className="side-bar-items">
+        <div className="full-height">
+          <a href="" className="side-bar-link">
+            <div className=" side-bar-items-content d-flex justify-content-start align-items-center">
+              {this.props.content.logo}
+            </div>
+          </a>
+        </div>
+      </div>
+    );
+  }
 }
- 
+
 export default PermanentSideBar;
